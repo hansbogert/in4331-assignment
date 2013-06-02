3dc14480edecd221f70de665ed8cf3f0f3f23f33
Hans van den Bogert <hansbogert@gmail.com>
2013-06-02 14:52:18 +0200

    Added more filters and css to project1

--- project1/index.php
+++ project1/index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="bootstrap.min.css"/>
     <title>Movie browser!</title>
     <script src="http://code.jquery.com/jquery-2.0.0.min.js" type="text/javascript"></script>
     <script src="xml.js" type="text/javascript"> </script>
   </head>
   <body>
     <form id="form">
-      <input type="text" name="title" value="title" id="title"/><br/>
+      <input type="text" name="title" placeholder="title" id="title"/><br/>
       <select name="genre" id="genre">
 	<option value="">Genre!</option>
 	<option value="Action">Action</option>
@@ -15,7 +16,18 @@
 	<option value="Drama">Drama</option>
 	<option value="Western">Western</option>
       </select><br/>
-      <input type="text" name="year" value="" placeholder="year" id="year" />
+      <input type="text" name="year" value="" placeholder="year" id="year" /><br/>
+      <input type="text" name="keyword" value="" placeholder="summary keyword" id="keyword" /><br/>
+      <select id="actorList" >
+	<option value="" >
+	  Choose actor
+	</option>
+      </select>
+      <select id="directorList" >
+	<option value="" >
+	  Choose director
+	</option>
+      </select>
       <input type="button" value="search" onclick="updateList()">
     </form>
     <div id="results">
--- project1/xml.js
+++ project1/xml.js
@@ -1,18 +1,41 @@
 $(init)
-function init()
-{
+function init(){
     $('#form').change(updateList)
     $("input[type='text']").change(updateList());
+    retrieveActors(presentActors)
+    retrieveDirectors(presentDirectors)
     console.log("starting")
     updateList();
 }
 
-function getXml(query, presenter)
-{
+function getFullName(actorNode){
+	return $.trim($(actorNode).find('first_name').text()) +" "+ $.trim($(actorNode).find('last_name').text())
+}
+
+function presentActors(data){
+    presentArtist(data,"actor", "#actorList");
+}
+
+function presentDirectors(data){
+    presentArtist(data, "director", "#directorList");
+}
+
+function presentArtist(data, type, where){
+    artist = $(data).find(type);
+    artist.each(function(index,elem){
+	
+	$(where).append("<option \
+data-first-name='"+$(elem).find('first_name').text()+"' \
+data-last-name='"+$(elem).find('last_name').text()+"' \
+ value=\""+getFullName(elem)+"\">"+ getFullName(elem) +"</option>");
+    });
+}
+
+function getXml(query, presenter){
     proxyUrl="ba-simple-proxy.php?mode=native&url="
-    urlQuery = 'http://localhost:8081/exist/rest/db/movies-query?_query='+query;
+    urlQuery = 'http://217.23.11.60:8180/exist/rest/db/movies?_howmany=99%26_query='+query;
     getUrl = proxyUrl+urlQuery;
-    console.log("request: "+ getUrl )
+    //console.log("request: "+ getUrl )
     $.get(getUrl, presenter);
 }
 
@@ -20,32 +43,55 @@ function presentList(data)
 {
     htmlList = $('<ul></ul>')
     movies = $(data).find('movie');
-    console.log(movies);
     movies.each(function(index,elem){
-	htmlList.append('<li>'+ $(elem).children('title').text()+' - '+$(elem).children('year').text()+'</li>' )
+	htmlList.append('<li class="movie">'+ $(elem).children('title').text()+' - '+$(elem).children('year').text()+'<div class="summary">'+ $(elem).children('summary').text() +'</div></li>' )
     })
     $('#results').html(htmlList)
+    $('.movie').click(showSummary)
+}
+function showSummary(){
+    $('.summary').hide();
+    $(this).children('div').show();
 }
 
-function updateList()
-{
+function updateList(){
     getXml(formListQuery(), presentList )
 }
 
 
 
-function formListQuery()
-{
-    console.log('still alive')
-    query = "//movie[contains(title, '" + $('#title').val()+"') \
-and contains(genre,'"+$('#genre').val()+"') \
-and contains(year,'"+$('#year').val()+"')]";
-    console.log(query);
+function formListQuery(){
+
+    query = "doc('movies.xml')//movie[contains(title, '" + $('#title').val()+"') \
+and contains(genre,'"+$('#genre').val()+"')";
+
+    if($('#keyword').val().length > 0)
+	query += "and contains(lower-case(summary),'"+$('#keyword').val()+"') ";
+    if($('#actorList').val().length > 0){
+
+	option =  $('#actorList option:selected');
+	firstName = option.data('first-name');
+	lastName = option.data('last-name');
+	query += "and (actor/first_name = '"+firstName+"' and actor/last_name = '"+lastName+"')";
+    }
+
+    if($('#directorList').val().length > 0){
+        option =  $('#directorList option:selected');
+        firstName = option.data('first-name');
+        lastName = option.data('last-name');
+	query += "and (director/first_name = '"+firstName+"' and director/last_name = '"+lastName+"')";
+    }
+
+    query += "and contains(year,'"+$('#year').val()+"')]";
     return query;
 }
 
-function retrieveMovie(title, presenter)
-{
-    query = "//movie[title = '+title+']";
-    getXml(query, presenter);
+function retrieveActors(presenter){
+    query = "doc('movies.xml')//actor";
+    getXml(query, presenter)
+}
+
+function retrieveDirectors(presenter){
+    query = "doc('movies.xml')//director";
+    getXml(query,presenter);
 }
