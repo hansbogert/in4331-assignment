7a2819dfbb3e3106da7523ef353f8fbb2ffead2e
Hans van den Bogert <hansbogert@gmail.com>
2013-06-26 15:42:28 +0200

    better startup -debian style

--- chapter19/provision.sh
+++ chapter19/provision.sh
@@ -10,18 +10,29 @@ cd /vagrant/res; bash download-resources.sh
 cd
 
 # check if hadoop is already installed
-dpkg --status hadoop
-if [ $? = 1 ]
+notInstalled=`dpkg --status hadoop |grep not-installed |wc -l`
+if [ $notInstalled = 1 ]
 then
 	if [ ! -f hadoop_1.1.2-1_i386.deb ]
 	then
-		wget http://apache.xl-mirror.nl/hadoop/common/hadoop-1.1.2/hadoop_1.1.2-1_i386.deb
+		wget --progress=dot:mega http://apache.xl-mirror.nl/hadoop/common/hadoop-1.1.2/hadoop_1.1.2-1_i386.deb
 	fi
 	sudo dpkg -i hadoop_1.1.2-1_i386.deb
 
 	#set java home correctly
 	sudo sed -i -e 's/\(export JAVA_HOME=\).*/\1\/usr\/lib\/jvm\/java-7-openjdk-i386/' /etc/hadoop/hadoop-env.sh 
-
+	cat << 'EOF' | sudo tee /etc/hadoop/core-site.xml
+<configuration>
+<property>
+    <name>fs.default.name</name>
+    <value>hdfs://localhost:9000</value>
+  </property>
+</configuration>
+EOF
+        sudo su hdfs -c 'hadoop namenode -format'
+	sudo invoke-rc.d hadoop-namenode start
+	sudo invoke-rc.d hadoop-datanode start
+        
 fi
 
 ## download java
