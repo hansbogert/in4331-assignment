155201068c69847c5f6fe44b8324e4b1118f14f2
Hans van den Bogert <hansbogert@gmail.com>
2013-06-26 13:08:57 +0200

    set java_home correct after install (what a stupid default)

--- chapter19/provision.sh
+++ chapter19/provision.sh
@@ -17,4 +17,9 @@ then
 		wget http://apache.xl-mirror.nl/hadoop/common/hadoop-1.1.2/hadoop_1.1.2-1_i386.deb
 	fi
 	sudo dpkg -i hadoop_1.1.2-1_i386.deb
+
+	#set java home correctly
+	sudo sed -i -e 's/\(export JAVA_HOME=\).*/\1\/usr\/lib\/jvm\/java-7-openjdk-i386/' /etc/hadoop/hadoop-env.sh 
+
 fi
+
